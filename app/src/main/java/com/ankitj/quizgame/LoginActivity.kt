fcaeac9c3777c32752684ee54b32ca1ff456984a
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-10 01:26:56 +0530

    Added ViewBinding to the LoginActivity

--- app/src/main/java/com/ankitj/quizgame/LoginActivity.kt
+++ app/src/main/java/com/ankitj/quizgame/LoginActivity.kt
@@ -1,20 +1,44 @@
 package com.ankitj.quizgame
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ankitj.quizgame.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var loginActivityBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+
+        loginActivityBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(loginActivityBinding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        loginActivityBinding.buttonSignIn.setOnClickListener {
+
+        }
+
+        loginActivityBinding.buttonGoogleSignIn.setOnClickListener {
+
+        }
+
+        loginActivityBinding.textViewSignUp.setOnClickListener {
+            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginActivityBinding.textViewForgotPassword.setOnClickListener {
+
         }
     }
 }
