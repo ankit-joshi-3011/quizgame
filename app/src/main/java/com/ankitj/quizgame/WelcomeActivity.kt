37a896718af13293d9137e636ab18d8f64621d99
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-09 20:39:38 +0530

    Used view binding in the WelcomeActivity

--- app/src/main/java/com/ankitj/quizgame/WelcomeActivity.kt
+++ app/src/main/java/com/ankitj/quizgame/WelcomeActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ankitj.quizgame.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var welcomeActivityBinding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_welcome)
+
+        welcomeActivityBinding = ActivityWelcomeBinding.inflate(layoutInflater);
+        setContentView(welcomeActivityBinding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
