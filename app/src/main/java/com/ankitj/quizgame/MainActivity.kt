f0a5b9a24d388f3c1d3d46dc62148efb2dd3fe8b
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-10 20:53:02 +0530

    Added the design and logic to sign out the user

--- app/src/main/java/com/ankitj/quizgame/MainActivity.kt
+++ app/src/main/java/com/ankitj/quizgame/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.ankitj.quizgame
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ankitj.quizgame.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainActivityBinding: ActivityMainBinding
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBinding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        mainActivityBinding.buttonSignOut.setOnClickListener {
+            auth.signOut()
+
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
         }
     }
 }
