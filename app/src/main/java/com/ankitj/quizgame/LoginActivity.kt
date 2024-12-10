db65c2df3eae9ebe7bad7ee50249c1834b761d55
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-11 01:02:39 +0530

    Added the logic to sign in with a Google account

--- app/src/main/java/com/ankitj/quizgame/LoginActivity.kt
+++ app/src/main/java/com/ankitj/quizgame/LoginActivity.kt
@@ -1,19 +1,32 @@
 package com.ankitj.quizgame
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ankitj.quizgame.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginActivityBinding: ActivityLoginBinding
     private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +47,15 @@ class LoginActivity : AppCompatActivity() {
             signInWithFirebase(email, password)
         }
 
-        loginActivityBinding.buttonGoogleSignIn.setOnClickListener {
+        registerActivityForGoogleSignIn()
 
+        val textOfGoogleSignInButton = loginActivityBinding.buttonGoogleSignIn.getChildAt(0) as TextView
+        textOfGoogleSignInButton.text = getString(R.string.google_sign_in_button_text)
+        textOfGoogleSignInButton.setTextColor(Color.BLACK)
+        textOfGoogleSignInButton.textSize = 18F
+
+        loginActivityBinding.buttonGoogleSignIn.setOnClickListener {
+            signInWithGoogle()
         }
 
         loginActivityBinding.textViewSignUp.setOnClickListener {
@@ -72,6 +92,46 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this@LoginActivity, getText(R.string.login_failure_text), Toast.LENGTH_SHORT).show()
                 Log.e("LoginActivity", getString(R.string.login_failure_text), task.exception)
             }
+        }
+    }
+
+    private fun signInWithGoogle() {
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+        val signInIntent: Intent = googleSignInClient.signInIntent
+        activityResultLauncher.launch(signInIntent)
+    }
+
+    private fun registerActivityForGoogleSignIn() {
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val resultCode = result.resultCode
+            val resultData = result.data
+
+            if (resultCode == RESULT_OK && resultData != null) {
+                val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(resultData)
+                firebaseSignInWithGoogle(task)
+            }
+        }
+    }
+
+    private fun firebaseSignInWithGoogle(task: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount = task.getResult(ApiException::class.java)
+            Toast.makeText(this@LoginActivity, getText(R.string.login_successful_text), Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+            val authCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+            auth.signInWithCredential(authCredential)
+        } catch (e: ApiException) {
+            Toast.makeText(this@LoginActivity, getText(R.string.login_failure_text), Toast.LENGTH_SHORT).show()
+            Log.e("LoginActivity", getString(R.string.login_failure_text), e)
         }
     }
 }
