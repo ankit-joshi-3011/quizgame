ecd2ddd8e061eca51a03aa8c940a249c81c5c406
Ankit Joshi <ankit.joshi2406@gmail.com>
2024-12-11 01:20:10 +0530

    Added a button to start the QuizActivity. Also, added to code to sign out a user who was logged in through their Google account

--- app/src/main/java/com/ankitj/quizgame/MainActivity.kt
+++ app/src/main/java/com/ankitj/quizgame/MainActivity.kt
@@ -2,11 +2,14 @@ package com.ankitj.quizgame
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ankitj.quizgame.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -26,8 +29,25 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        mainActivityBinding.buttonStartQuiz.setOnClickListener {
+            val intent = Intent(this@MainActivity, QuizActivity::class.java)
+            startActivity(intent)
+        }
+
         mainActivityBinding.buttonSignOut.setOnClickListener {
             auth.signOut()
+
+            val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+
+            val googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+
+            googleSignInClient.signOut().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, getText(R.string.sign_out_successful_text), Toast.LENGTH_SHORT).show()
+                }
+            }
 
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
